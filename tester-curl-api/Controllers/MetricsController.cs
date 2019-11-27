97f0515b6b42fd30d3f7309b27ee3790ed2b761d
Thiago Lopes <thiago@umbler.com>
2019-11-27 01:23:49 -0300

    a big refactoring on curl api + dockerfile

--- tester-curl-api/Controllers/MetricsController.cs
+++ tester-curl-api/Controllers/MetricsController.cs
@@ -1,21 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Diagnostics;
 using System.Text.RegularExpressions;
-using System.Text;
 
 namespace tester_curl_api.Controllers
 {
-    public class Output
-    {
-        public string SiteUp { get; set; }
-        public string SiteUpRaw { get; set; }
-        public string SiteStatusCode { get; set; }
-    }
 
     [ApiController]
     [Route("[controller]")]
@@ -28,129 +17,82 @@ namespace tester_curl_api.Controllers
             _logger = logger;
         }
 
-        private string FormatSiteError(Match match)
-        {
-            int status = int.Parse(match.Groups["status"].Value);
-
-            StringBuilder sb = new StringBuilder();
-            var name = "site_up_raw";
-            sb.AppendLine($"# TYPE {name} gauge");
-            sb.AppendLine($"{name} {(status > 299 ? "0" : "1")}");
-
-            name = "site_status_code";
-            sb.AppendLine($"# TYPE {name} gauge");
-            sb.AppendLine($"{name} {status}");
-
-            return sb.ToString();
-        }
-
-        private string FormatNetworkError(string stdErr)
-        {
-            StringBuilder sb = new StringBuilder();
-
-            var name = "site_up";
-            sb.AppendLine($"# TYPE {name} gauge");
-            sb.AppendLine($"{name}{{response=\"{stdErr.Replace("\n", "").Trim()}\"}} 0");
-
-            name = "site_up_raw";
-            sb.AppendLine($"# TYPE {name} gauge");
-            sb.AppendLine($"{name} 0");
-
-            name = "site_status_code";
-            sb.AppendLine($"# TYPE {name} gauge");
-            sb.AppendLine($"{name} 2");
-
-            return sb.ToString();
-        }
-
-        private string FormatUnknowError()
-        {
-            StringBuilder sb = new StringBuilder();
-
-            var name = "site_up";
-            sb.AppendLine($"# TYPE {name} gauge");
-            sb.AppendLine($"{name}{{response=\"unknow error\"}} 0");
-
-            name = "site_up_raw";
-            sb.AppendLine($"# TYPE {name} gauge");
-            sb.AppendLine($"{name} 0");
-
-            name = "site_status_code";
-            sb.AppendLine($"# TYPE {name} gauge");
-            sb.AppendLine($"{name} 3");
-
-            return sb.ToString();
-        }
-
         [HttpGet]
         public string Get(string url)
         {
             var escapedArgs = "-i -L -S --stderr - http://" + url;
 
-            var process = new Process
+            using var process = new Process
             {
                 StartInfo = new ProcessStartInfo
                 {
                     FileName = "curl",
                     Arguments = escapedArgs,
-                    RedirectStandardOutput = true,
-                    RedirectStandardError = true,
-                    UseShellExecute = false,
                     CreateNoWindow = true,
+                    UseShellExecute = false,
+                    RedirectStandardError = true,
+                    RedirectStandardOutput = true,
                 }
             };
 
             process.Start();
             string result = process.StandardOutput.ReadToEnd();
-            string error = process.StandardError.ReadToEnd();
             process.WaitForExit(61000);
-            System.Threading.Thread.Sleep(2000);
-            string error1 = process.StandardError.ReadToEnd();
 
-            bool flag_error = string.IsNullOrWhiteSpace(result);
+            var obj = new Output();
 
-            if (flag_error)
-                return FormatUnknowError();
+            if (string.IsNullOrWhiteSpace(result))
+                return obj.SetError("2", "completely empty response");
 
             var match = Regex.Match(result, @"(curl:)\s(\(\d+\))(.*)(\n)");
             if (match.Success)
-                return FormatNetworkError(match.Value);
+                return obj.SetError("3", match.Value);
 
-            var headersEnd = result.IndexOf("\n\n\n");
-            if (headersEnd == -1)
-            {
-                headersEnd = result.IndexOf('<');
-                if (headersEnd == -1)
-                    flag_error = true;
-            }
+            //var headersEnd = GetIndexOfHeadersEnd(result);
 
-            string headers = result.Substring(0, headersEnd);
+            //string headers = headersEnd == -1 ? result : result.Substring(0, headersEnd);
 
-            match = Regex.Match(headers, @"HTTP\/(.*)\s(?<status>\d{3,3})", RegexOptions.RightToLeft);
-
-            StringBuilder sb = new StringBuilder();
-            var name = "site_up_raw";
-            //sb.AppendLine($"# HELP {name} TRL");
-            sb.AppendLine($"# TYPE {name} gauge");
+            match = Regex.Match(result, @"HTTP\/.*\s(?<status>\d{3,3})\s(?<response>.*)", RegexOptions.RightToLeft); //I've been used to use the headers insted of result until now.
+            //but thinking twice, i guess we will the same result;
 
             if (match.Success)
             {
-                int status = int.Parse(match.Groups["status"].Value);
-                sb.AppendLine($"{name} {(status > 299 ? "0" : "1")}");
-
-                name = "site_status_code";
-                sb.AppendLine($"# TYPE {name} gauge");
-                sb.AppendLine($"{name} {status}");
+                obj.SetStatusCode(match);
+                EvaluateTitleCondition(obj, result);
+                return obj.ToString();
             }
             else
             {
-                sb.AppendLine($"{name} 2");
+                return obj.SetError("3", "status code not found");
             }
+        }
 
-            return sb.ToString();
+        public int GetIndexOfHeadersEnd(string result)
+        {
+            var headersEnd = result.IndexOf("\n\n\n");
+
+            if (headersEnd == -1)
+                headersEnd = result.IndexOf("\r\n\r\n");
+
+            if (headersEnd == -1)
+                headersEnd = result.IndexOf('<');
+
+            return headersEnd;
+        }
+
+        public void EvaluateTitleCondition(Output obj, string result)
+        {
+            if (obj.IsSiteUp == "0")
+            {
+                var match = Regex.Match(result, @"(\<title.*\>)(?<title>(?<status>5\d\d).*)(\<)", RegexOptions.IgnoreCase);
+                if (match.Success)
+                {
+                    var status = match.Groups["status"].Value;
+                    var response = match.Groups["title"].Value;
+                    obj.SetError(status, response);
+                }
+
+            }
         }
     }
 }
-
-//var statuses = Regex.Matches(headers, @"HTTP\/(.*)\s(?<status>\d{3,3})");
-//statuses[statuses.Count - 1].Groups["status"].Value
---
+++ tester-curl-api/Controllers/Output.cs
@@ -0,0 +1,55 @@
+﻿using System.Text.RegularExpressions;
+using System.Text;
+
+namespace tester_curl_api.Controllers
+{
+    public class Output
+    {
+        public string SiteUp { get; set; }
+        public string Response { get; set; }
+        public string SiteStatusCode { get; private set; }
+        public string IsSiteUp => int.Parse(SiteStatusCode) >= 200 && int.Parse(SiteStatusCode) < 300 ? "1" : "0";
+
+        public void SetStatusCode(Match match)
+        {
+            Response = match.Groups["response"].Value;
+            SiteStatusCode = match.Groups["status"].Value;
+        }
+
+        public void SetOtherStatusBasedOnStatusCode()
+        {
+            int status = GetStatusCode();
+            SiteUp = status >= 200 && status < 300 ? "1" : "0";
+            Response = status >= 200 && status < 300 ? "OK" : "NOK";
+        }
+
+        public string SetError(string statusCode, string response)
+        {
+            SiteUp = "0";
+            Response = response;
+            SiteStatusCode = statusCode;
+            return ToString();
+        }
+
+        public int GetStatusCode()
+        {
+            return int.Parse(SiteStatusCode);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"# TYPE site_up gauge");
+            sb.AppendLine($"site_up{{response=\"{Response.Replace("\n", "").Trim()}\"}} " + IsSiteUp);
+
+            sb.AppendLine($"# TYPE site_up_raw gauge");
+            sb.AppendLine($"site_up_raw " + IsSiteUp);
+
+            sb.AppendLine($"# TYPE site_status_code gauge");
+            sb.AppendLine($"site_status_code " + SiteStatusCode);
+
+            return sb.ToString();
+        }
+    }
+}
